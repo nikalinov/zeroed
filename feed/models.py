7e0a310efe0d9de5c732fe2bdf7fcbc528ca5259
nikalinov <nikitakalinovskij@gmail.com>
2023-06-16 15:20:27 +0200

    Improve and add some models

--- feed/models.py
+++ feed/models.py
@@ -1,4 +1,4 @@
-import datetime
+from django.utils.timezone import now
 from django.db import models
 from feed.countries import COUNTRIES
 from django.urls import reverse
@@ -7,7 +7,7 @@ from django.urls import reverse
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    country = models.CharField(max_length=100, choices=COUNTRIES)
+    country = models.CharField(max_length=100, choices=COUNTRIES, default='US')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -23,11 +23,23 @@ class ContentType(models.Model):
         return f'{self.type}'
 
 
+class Language(models.Model):
+    language = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.language}'
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.RESTRICT)
-    post_date = models.DateField(default=datetime.date.today())
+    post_date = models.DateField(default=now)
     content_type = models.ManyToManyField(ContentType)
+    language = models.ForeignKey(Language, on_delete=models.RESTRICT)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-post_date']
 
     def __str__(self):
         return f'{self.title}'
