f62060d76f2d9986d3f3cd7a20511cb7139bc04b
nikalinov <nikitakalinovskij@gmail.com>
2023-06-17 18:28:19 +0200

    Add home/index page

--- feed/views.py
+++ feed/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from .models import Language, Blog, Author, Comment, ContentType
 
 
-def index():
-    pass
+def index(request):
+    """View function for home page of site."""
+
+    num_blogs = Blog.objects.count()
+    num_authors = Author.objects.count()
+    num_comments = Comment.objects.count()
+    num_blogs_with_us_authors = Blog.objects.filter(author__country__exact='US').count()
+    num_how_blogs = Blog.objects.filter(title__icontains='how').count()
+
+    context = {
+        'num_blogs': num_blogs,
+        'num_authors': num_authors,
+        'num_comments': num_comments,
+        'num_blogs_with_us_authors': num_blogs_with_us_authors,
+        'num_how_blogs' : num_how_blogs,
+    }
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
 
 
 class AuthorListView(ListView):
@@ -21,4 +38,3 @@ class BlogListView(ListView):
 
 class BlogDetailView(DetailView):
     pass
-
